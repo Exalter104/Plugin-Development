7454a8845695479db6088f517edd0ed3a4b6a250
exalter104 <sananjani104@gmail.com>
2025-02-13 21:00:52 +0500

    Your commit message

--- is-set/is_set.php
+++ is-set/is_set.php
@@ -2,9 +2,7 @@
 // is-set : is-set true  if a variable is declared and not null
 $exalter = "Sheraz is a exalter";
 if (isset($exalter)) {
-    echo " yes i am exalter";
+    echo " YES i am exalter";
 
     # code...
-} else {
-    echo "no i am not exalter";
 }
