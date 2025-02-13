7731c1f1acd3200a46a1ec4a95bde367c7c0571a
exalter104 <sananjani104@gmail.com>
2025-02-13 20:48:31 +0500

    Is Set Function : is set function with example

---
+++ is-set/is_set.php
@@ -0,0 +1,10 @@
+<?php
+// is-set : is-set true  if a variable is declared and not null
+$exalter = "Sheraz is a exalter";
+if (isset($exalter)) {
+    echo " yes i am exalter";
+
+    # code...
+} else {
+    echo "no i am not exalter";
+}
